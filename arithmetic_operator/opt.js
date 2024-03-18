7aea7213ca26014949f31f12d8c6e53036900bd0
ameerhamzashah <160713923+ameerhamzashah1@users.noreply.github.com>
2024-03-18 08:31:47 +0500

    Arithmetic_operators
    these are my assignmnet of ts nd js

---
+++ arithmetic_operator/opt.js
@@ -0,0 +1,48 @@
+// ARITHMETIC OPERATORS:
+// 1 Additon:
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+console.log(num1 + num2);
+console.log(num2 + num3);
+var num123 = num1 + num2 + num3;
+console.log(num123);
+console.log(5 + 5 + 10 + 80);
+// Subtraction:
+var n1 = 50;
+var n2 = 60;
+var n3 = 40;
+console.log(n1 - n2);
+console.log(n2 - n3);
+var n123 = n1 - n2 - n3;
+console.log(n123);
+console.log(7 - 7 - 9);
+//Multiplication:
+var x1 = 5;
+var x2 = 6;
+var x3 = 12;
+console.log(x1 * x2);
+console.log(x3 * x1);
+var x123 = 2 * 3;
+console.log(x123);
+console.log(5 * 5 * 10);
+//Division :
+var d1 = 15;
+var d2 = 5;
+var d3 = 4;
+console.log(d1 / d2 / d3);
+console.log(d3 / d2);
+var d123 = d1 / d2 / d3;
+console.log(d123);
+console.log(15 / 15);
+//Exponentional:
+var e = 2;
+var ex = 5;
+console.log(Math.pow(ex, 2));
+console.log(Math.pow(e, 2));
+console.log(Math.pow(2, 4));
+//Modules:
+var m = 10;
+var m1 = 5;
+console.log(m1 % m);
+console.log(m % m1);
---
+++ arithmetic_operator/opt.ts
@@ -0,0 +1,56 @@
+// ARITHMETIC OPERATORS:
+
+// 1 Additon:
+
+let num1 = 10;  let num2 = 20;  let num3 = 30;
+console.log(num1+num2); 
+console.log(num2+num3);
+let num123 = num1+num2+num3;
+console.log(num123);
+console.log(5+5+10+80);
+
+// Subtraction:
+
+let n1 = 50;  let n2 = 60;  let n3 = 40;
+console.log(n1-n2); 
+console.log(n2-n3);
+let n123 = n1-n2-n3;
+console.log(n123);
+console.log(7-7-9);
+
+//Multiplication:
+
+let x1 = 5;  let x2 = 6;  let x3 = 12;
+console.log(x1*x2); 
+console.log(x3*x1);
+let x123 = 2 * 3;
+console.log(x123);
+console.log(5*5*10);
+
+//Division :
+
+let d1 = 15;  let d2 = 5;  let d3 = 4;
+console.log(d1/d2/d3); 
+console.log(d3/d2);
+let d123 = d1/d2/d3;
+console.log(d123);
+console.log(15/15);
+
+//Exponentional:
+
+let e = 2;  
+let ex = 5;
+console.log(ex**2);
+console.log(e**2);
+console.log(2**4);
+
+//Modules:
+
+let m = 10;
+let m1 = 5;
+console.log(m1 % m);
+console.log(m % m1);
+
+
+
+
